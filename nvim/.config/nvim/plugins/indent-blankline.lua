053d39b6788a3ba2383f18d2e9132549464200b4
Ilya Timofeev <il.tmfv@gmail.com>
2021-12-29 17:40:40 +0300

    Adjust blankline settings

--- nvim/.config/nvim/plugins/indent-blankline.lua
+++ nvim/.config/nvim/plugins/indent-blankline.lua
@@ -3,5 +3,8 @@ vim.opt.listchars:append("space:⋅")
 
 require("indent_blankline").setup {
     show_first_indent_level = false,
+    show_current_context = true,
     space_char_blankline = " ",
+    bufname_exclude = {'NeogitStatus'},
+    filetype_exclude = {'help', 'NeogitStatus'},
 }
