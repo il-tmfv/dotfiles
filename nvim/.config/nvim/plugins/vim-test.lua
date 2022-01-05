eed98c331ec168d50e1b7993897201a42e77fafb
Ilya Timofeev <il.tmfv@gmail.com>
2022-01-05 10:56:10 +0300

    vim-test config in lua

---
+++ nvim/.config/nvim/plugins/vim-test.lua
@@ -0,0 +1,5 @@
+vim.api.nvim_set_keymap('n', '<leader>tn', ':TestNearest<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>tf', ':TestFile<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>ts', ':TestSuite<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>tl', ':TestLast<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>tv', ':TestVisit<CR>', { noremap = true, silent = true })
