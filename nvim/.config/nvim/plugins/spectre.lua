bfe5fd26d6577ae91acd1334b6d0f35ff69b7df6
Ilya Timofeev <il.tmfv@gmail.com>
2022-01-05 10:45:30 +0300

    Spectre config in lua

---
+++ nvim/.config/nvim/plugins/spectre.lua
@@ -0,0 +1,7 @@
+vim.api.nvim_set_keymap("n", "<leader>S", ":lua require('spectre').open()<CR>", { noremap = true })
+-- search current word
+vim.api.nvim_set_keymap("n", "<leader>sw", ":lua require('spectre').open_visual({select_word=true})<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>s", ":lua require('spectre').open_visual()<CR>", { noremap = true })
+
+-- search in current file
+vim.api.nvim_set_keymap("n", "<leader>sp", "viw:lua require('spectre').open_file_search()<cr>", { noremap = true })
