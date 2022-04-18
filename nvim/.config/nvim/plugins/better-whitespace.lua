0212a6dc12cb6059da995a4a2c6a60fb0b702a6b
Ilya Timofeev <me@tmfv.ru>
2022-04-18 10:16:23 +0300

    Remove better_whitespace workaround

--- nvim/.config/nvim/plugins/better-whitespace.lua
+++ nvim/.config/nvim/plugins/better-whitespace.lua
@@ -2,5 +2,3 @@ vim.g.better_whitespace_enabled = 1
 vim.g.strip_whitespace_confirm = 0
 vim.g.strip_whitespace_on_save = 1
 vim.g.strip_only_modified_lines = 1
--- https://github.com/ntpeters/vim-better-whitespace/issues/127#issuecomment-899600717
-vim.cmd("autocmd BufWritePre * :StripWhitespaceOnChangedLines")
