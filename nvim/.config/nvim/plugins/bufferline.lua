2680065ded25a3e1fd37a0a06a6144d8b6c6ec03
Ilya Timofeev <me@tmfv.ru>
2022-12-14 18:15:15 +0300

    Add groups to bufferline

--- nvim/.config/nvim/plugins/bufferline.lua
+++ nvim/.config/nvim/plugins/bufferline.lua
@@ -1,7 +1,34 @@
 require("bufferline").setup({
   options = {
     offsets = {{filetype = "NvimTree", text = "File Explorer", text_align = "left"}},
-    diagnostics = "coc"
+    diagnostics = "coc",
+    groups = {
+      options = {
+        toggle_hidden_on_enter = true -- when you re-enter a hidden group this options re-opens that group so the buffer is visible
+      },
+      items = {
+        {
+          name = "Tests", -- Mandatory
+          highlight = {underline = true, sp = "blue"}, -- Optional
+          priority = 2, -- determines where it will appear relative to other groups (Optional)
+          icon = "", -- Optional
+          matcher = function(buf) -- Mandatory
+            return buf.filename:match('%_test') or buf.filename:match('%_spec')
+          end,
+        },
+        {
+          name = "Docs",
+          highlight = {undercurl = true, sp = green},
+          auto_close = false,  -- whether or not close this group if it doesn't contain the current buffer
+          matcher = function(buf)
+            return buf.filename:match('%.md') or buf.filename:match('%.txt')
+          end,
+          separator = { -- Optional
+            style = require('bufferline.groups').separator.tab
+          },
+        }
+      }
+    }
   }
 })
 
