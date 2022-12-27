eb9e8e6a36441a957c6b893c8ca5b716a5ed4d93
Ilya Timofeev <me@tmfv.ru>
2022-12-27 10:45:09 +0300

    Add more fzf mappings

--- nvim/.config/nvim/plugins/fzf.lua
+++ nvim/.config/nvim/plugins/fzf.lua
@@ -5,6 +5,8 @@ require('fzf-lua').setup{
     }
   }
 }
+vim.api.nvim_set_keymap('n', '<leader>/', "<cmd>lua require('fzf-lua').blines()<cr>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>fj', "<cmd>lua require('fzf-lua').buffers()<cr>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>ff', "<cmd>lua require('fzf-lua').files()<cr>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>fg', "<cmd>lua require('fzf-lua').grep_project()<cr>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>fw', "<cmd>lua require('fzf-lua').grep_cword()<cr>", { noremap = true, silent = true })
