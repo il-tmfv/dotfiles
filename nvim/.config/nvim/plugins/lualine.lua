f15259f9b4f9bb82ef9ee7df49fb0974203bef32
Ilya Timofeev <me@tmfv.ru>
2022-12-27 10:34:25 +0300

    Show relative path in lualine

--- nvim/.config/nvim/plugins/lualine.lua
+++ nvim/.config/nvim/plugins/lualine.lua
@@ -3,4 +3,12 @@ require'lualine'.setup {
     icons_enabled = true,
     theme = 'nord',
   },
+  sections = {
+    lualine_c = {
+      {
+        'filename',
+        path = 1
+      }
+    }
+  }
 }
