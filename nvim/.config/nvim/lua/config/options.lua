3b477b2c93d470c3c786be007be9ccf32322be2e
Ilya Timofeev <me@tmfv.ru>
2023-02-12 18:15:42 +0300

    Add options

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,3 +1,34 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.opt.showmode = false
+vim.opt.hlsearch = false
+vim.opt.hidden = true
+vim.opt.errorbells = false
+vim.opt.nu = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.wrap = false
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.writebackup = false
+vim.opt.undodir = "~/.vim/undodir"
+vim.opt.undofile = true
+vim.opt.incsearch = true
+vim.opt.scrolloff = 6
+vim.opt.colorcolumn = "80,120"
+vim.opt.signcolumn = "yes"
+vim.opt.cmdheight = 2
+vim.opt.updatetime = 300
+vim.opt.mouse = "a"
+vim.opt.termguicolors = true
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+vim.opt.foldlevelstart = 99
+vim.opt.foldnestmax = 3
+vim.opt.foldminlines = 1
+vim.opt.foldtext = [[substitute(getline(v:foldstart),'\\t',repeat('\ ',&tabstop),'g').'...'.trim(getline(v:foldend))]]
