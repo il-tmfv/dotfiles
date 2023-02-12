d607d0d2b277496add92db5ab134fd758c079d40
Ilya Timofeev <me@tmfv.ru>
2023-02-12 19:15:23 +0300

    Typo in comment

--- nvim/.config/nvim/plugins/ctrlsf.lua
+++ nvim/.config/nvim/plugins/ctrlsf.lua
@@ -3,5 +3,5 @@ vim.api.nvim_set_keymap("n", "<leader>S", "<Plug>CtrlSFPrompt", { noremap = fals
 vim.api.nvim_set_keymap("n", "<leader>sw", "<Plug>CtrlSFCwordPath", { noremap = false })
 vim.api.nvim_set_keymap("v", "<leader>sw", "<Plug>CtrlSFVwordPath", { noremap = false })
 
--- close spectre panel as window on the left
+-- close panel as window on the left
 vim.api.nvim_set_keymap("n", "<leader>sq", ":CtrlSFToggle<CR>", { noremap = true })
