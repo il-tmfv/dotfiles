60800d509d4759f6533dfc341f31bdbb1c91082d
Ilya Timofeev <me@tmfv.ru>
2023-04-15 09:57:58 +0300

    Define AC command for non-existent files

--- nvim/.config/nvim/lua/plugins/rails.lua
+++ nvim/.config/nvim/lua/plugins/rails.lua
@@ -1,3 +1,6 @@
 return {
-  'tpope/vim-rails'
+  'tpope/vim-rails',
+  config = function()
+    vim.api.nvim_create_user_command("AC", [[execute "e " . eval('rails#buffer().alternate()')]], {})
+  end
 }
