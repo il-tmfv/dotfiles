15169cfc558410a06e20a7769fd75a45af3e308f
Ilya Timofeev <me@tmfv.ru>
2023-04-22 10:52:49 +0300

    Add rubocop format commands

--- nvim/.config/nvim/lua/plugins/rails.lua
+++ nvim/.config/nvim/lua/plugins/rails.lua
@@ -2,5 +2,7 @@ return {
   'tpope/vim-rails',
   config = function()
     vim.api.nvim_create_user_command("AC", [[execute "e " . eval('rails#buffer().alternate()')]], {})
+    vim.api.nvim_create_user_command("FR", [[!bundle exec rubocop -a %]], {})
+    vim.api.nvim_create_user_command("FRR", [[!bundle exec rubocop -A %]], {})
   end
 }
