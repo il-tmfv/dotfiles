0c9b2bc3930097d0ae953cb699629a24a4c14f2a
Тимофеев Илья Игоревич <ilya.timofeev@sbermarket.ru>
2023-06-02 15:23:38 +0300

    Switch to dip

--- nvim/.config/nvim/lua/plugins/rails.lua
+++ nvim/.config/nvim/lua/plugins/rails.lua
@@ -2,7 +2,7 @@ return {
   'tpope/vim-rails',
   config = function()
     vim.api.nvim_create_user_command("AC", [[execute "e " . eval('rails#buffer().alternate()')]], {})
-    vim.api.nvim_create_user_command("FR", [[!bundle exec rubocop -a %]], {})
-    vim.api.nvim_create_user_command("FRR", [[!bundle exec rubocop -A %]], {})
+    vim.api.nvim_create_user_command("FR", [[!dip rubocop -a %]], {})
+    vim.api.nvim_create_user_command("FRR", [[!dip rubocop -A %]], {})
   end
 }
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -202,5 +202,5 @@ export NVM_DIR="$HOME/.nvm"
 
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 export PATH="/opt/homebrew/opt/libpq/bin:$PATH"
-export DIP_FILE=/Users/timofeev/projects/instamart/dip_front.yml
+export DIP_FILE=/Users/timofeev/projects/storefront/dip_front.yml
 export PATH="/usr/local/opt/mysql@5.7/bin:$PATH"
