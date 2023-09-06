b8edd3ef7655cf117fe988d9c08eca91cff7212a
Ilya Timofeev <me@tmfv.ru>
2023-09-06 15:55:12 +0300

    Add TS support and remove rainbow

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -2,7 +2,6 @@ return {
   "nvim-treesitter/nvim-treesitter",
   event = { "BufReadPost", "BufNewFile" },
   dependencies = {
-    'p00f/nvim-ts-rainbow',
     'cuducos/yaml.nvim',
     'RRethy/nvim-treesitter-endwise',
   },
@@ -10,7 +9,20 @@ return {
   build = ":TSUpdate",
   opts = {
     -- A list of parser names, or "all"
-    ensure_installed = {"clojure", "ruby", "elixir", "javascript", "scss", "html", "yaml", "lua", "vim", "markdown"},
+    ensure_installed = {
+      "clojure",
+      "ruby",
+      "elixir",
+      "javascript",
+      "typescript",
+      "tsx",
+      "scss",
+      "html",
+      "yaml",
+      "lua",
+      "vim",
+      "markdown"
+    },
     -- Install languages synchronously (only applied to `ensure_installed`)
     sync_install = false,
     -- indent = {
@@ -39,17 +51,6 @@ return {
         enable = true,
         clear_on_cursor_move = true,
       },
-    },
-    rainbow = {
-      enable = true,
-      colors = {
-        '#0098e6', '#e16d6d', '#3fa455', '#c968e6', '#999', '#ce7e00'
-      },
-      -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
-      extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-      max_file_lines = nil, -- Do not enable for files with more than n lines, int
-      -- colors = {}, -- table of hex strings
-      -- termcolors = {} -- table of colour name strings
     },
     endwise = {
       enable = true,
