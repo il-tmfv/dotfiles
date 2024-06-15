b71e3ead1d43c65ffdb15c6cc74cc00df150b169
Ilya Timofeev <me@tmfv.ru>
2024-06-15 13:15:04 +0300

    Preselect the first item

--- nvim/.config/nvim/lua/plugins/cmp.lua
+++ nvim/.config/nvim/lua/plugins/cmp.lua
@@ -27,6 +27,12 @@ return {
 				end,
 			},
 
+			cmp.setup({
+				completion = {
+					completeopt = "menu,menuone,noinsert",
+				},
+			}),
+
 			mapping = cmp.mapping.preset.insert({
 				["<C-b>"] = cmp.mapping.scroll_docs(-4),
 				["<C-f>"] = cmp.mapping.scroll_docs(4),
