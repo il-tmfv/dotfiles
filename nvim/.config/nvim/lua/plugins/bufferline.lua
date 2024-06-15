c57513c8538634a1015285771d5c374845683988
Ilya Timofeev <me@tmfv.ru>
2024-06-15 11:50:50 +0300

    Draft

--- nvim/.config/nvim/lua/plugins/bufferline.lua
+++ nvim/.config/nvim/lua/plugins/bufferline.lua
@@ -1,16 +1,16 @@
 return {
-  'akinsho/bufferline.nvim',
-  dependencies = {
-    'nvim-tree/nvim-web-devicons'
-  },
-  lazy = false,
-  opts = {
-    options = {
-      offsets = {{filetype = "NvimTree", text = "File Explorer", text_align = "left"}},
-      diagnostics = "coc",
-    },
-  },
-  keys = {
-    { '<leader>bc', '<cmd>BufferLinePickClose<CR>', noremap = true }
-  },
+	"akinsho/bufferline.nvim",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+	lazy = false,
+	opts = {
+		options = {
+			offsets = { { filetype = "NvimTree", text = "File Explorer", text_align = "left" } },
+			diagnostics = "nvim_lsp",
+		},
+	},
+	keys = {
+		{ "<leader>bc", "<cmd>BufferLinePickClose<CR>", noremap = true },
+	},
 }
--- nvim/.config/nvim/lua/plugins/codeium.lua
+++ nvim/.config/nvim/lua/plugins/codeium.lua
@@ -1,23 +1,30 @@
 return {
-  'Exafunction/codeium.vim',
-  config = function ()
-    vim.keymap.set(
-      'n',
-      '<F12>',
-      function() return vim.fn['codeium#Chat']() end,
-      { expr = true, silent = true, desc = 'Start Codeium chat' }
-    )
-    vim.keymap.set(
-      'i',
-      '<F9>',
-      function() return vim.fn['codeium#CycleCompletions'](1) end,
-      { expr = true, silent = true, desc = 'Next Codeium completion' }
-    )
-    vim.keymap.set(
-      'i',
-      '<F10>',
-      function() return vim.fn['codeium#Clear']() end,
-      { expr = true, silent = true, desc = 'Clear Codeium suggestion' }
-    )
-  end
+	"Exafunction/codeium.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"hrsh7th/nvim-cmp",
+	},
+	config = function()
+		require("codeium").setup({})
+	end,
+	-- config = function ()
+	--   vim.keymap.set(
+	--     'n',
+	--     '<F12>',
+	--     function() return vim.fn['codeium#Chat']() end,
+	--     { expr = true, silent = true, desc = 'Start Codeium chat' }
+	--   )
+	--   vim.keymap.set(
+	--     'i',
+	--     '<F9>',
+	--     function() return vim.fn['codeium#CycleCompletions'](1) end,
+	--     { expr = true, silent = true, desc = 'Next Codeium completion' }
+	--   )
+	--   vim.keymap.set(
+	--     'i',
+	--     '<F10>',
+	--     function() return vim.fn['codeium#Clear']() end,
+	--     { expr = true, silent = true, desc = 'Clear Codeium suggestion' }
+	--   )
+	-- end
 }
