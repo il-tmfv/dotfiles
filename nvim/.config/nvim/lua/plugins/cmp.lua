4f064030a132b8faec58eb30d6c0fc878f06c6ce
Ilya Timofeev <me@tmfv.ru>
2024-06-15 13:12:42 +0300

    Fix snippets and cmp source order

--- nvim/.config/nvim/lua/plugins/cmp.lua
+++ nvim/.config/nvim/lua/plugins/cmp.lua
@@ -19,10 +19,10 @@ return {
 			snippet = {
 				-- REQUIRED - you must specify a snippet engine
 				expand = function(args)
-					vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+					-- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
 					-- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
 					-- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-					-- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+					vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
 					-- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
 				end,
 			},
@@ -36,8 +36,8 @@ return {
 			}),
 
 			sources = cmp.config.sources({
-				{ name = "nvim_lsp" },
 				{ name = "codeium" },
+				{ name = "nvim_lsp" },
 				-- { name = 'luasnip' }, -- For luasnip users.
 				{ name = "ultisnips" }, -- For ultisnips users.
 				-- { name = 'snippy' }, -- For snippy users.
