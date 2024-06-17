93a57f25889dac3365b3190579bfe09b3cf1502b
Ilya Timofeev <me@tmfv.ru>
2024-06-17 11:56:22 +0300

    More complex format_on_save logic

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -14,6 +14,21 @@ return {
 	opts = {
 		notify_on_error = false,
 		format_on_save = function(bufnr)
+			-- Disable autoformat on certain filetypes
+			local ignore_filetypes = { "sql", "java" }
+			if vim.tbl_contains(ignore_filetypes, vim.bo[bufnr].filetype) then
+				return
+			end
+			-- Disable with a global or buffer-local variable
+			if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+				return
+			end
+			-- Disable autoformat for files in a certain path
+			local bufname = vim.api.nvim_buf_get_name(bufnr)
+			if bufname:match("/node_modules/") then
+				return
+			end
+
 			-- Disable "format_on_save lsp_fallback" for languages that don't
 			-- have a well standardized coding style. You can add additional
 			-- languages here or re-enable it for the disabled ones.
