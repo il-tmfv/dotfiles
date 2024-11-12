cd4d22256e8a465b9bcddf9d1acc262967adda08
Ilya Timofeev <me@tmfv.ru>
2024-11-12 17:42:21 +0300

    Add line blame

--- nvim/.config/nvim/lua/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/plugins/gitsigns.lua
@@ -1,6 +1,15 @@
 return {
 	"lewis6991/gitsigns.nvim",
 	opts = {
+		current_line_blame = true,
+		current_line_blame_opts = {
+			virt_text = true,
+			virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+			delay = 1000,
+			ignore_whitespace = false,
+			virt_text_priority = 100,
+			use_focus = true,
+		},
 		on_attach = function(bufnr)
 			local gs = package.loaded.gitsigns
 
