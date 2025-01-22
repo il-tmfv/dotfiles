5882257014f3bcc435f46ca61279368d8c1dce4f
Ilya Timofeev <me@tmfv.ru>
2025-01-22 17:34:42 +0300

    Migrate to mini.splitjoin

--- nvim/.config/nvim/lua/plugins/ts-node-action.lua
+++
@@ -1,8 +0,0 @@
-return {
-  'ckolkey/ts-node-action',
-  dependencies = { 'nvim-treesitter' },
-  keys = {
-    { "S", "<cmd>lua require('ts-node-action').node_action()<CR>", desc = "Trigger Node Action" }
-  },
-  opts = {},
-}
