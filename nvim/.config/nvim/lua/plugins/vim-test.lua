5ffc98931d24ea5c2849ff220b0b6d3117e1c752
Ilya Timofeev <me@tmfv.ru>
2025-02-04 10:30:14 +0300

    Try vimux test strategy

--- nvim/.config/nvim/lua/plugins/vim-test.lua
+++ nvim/.config/nvim/lua/plugins/vim-test.lua
@@ -1,10 +1,15 @@
 return {
-  'vim-test/vim-test',
-  keys = {
-    { '<leader>tn', ':TestNearest<CR>', noremap = true, silent = true, desc = 'Test nearest line' },
-    { '<leader>tf', ':TestFile<CR>', noremap = true, silent = true, desc = 'Test file' },
-    { '<leader>ts', ':TestSuite<CR>', noremap = true, silent = true, desc = 'Test suite' },
-    { '<leader>tl', ':TestLast<CR>', noremap = true, silent = true, desc = 'Test last' },
-    { '<leader>tv', ':TestVisit<CR>', noremap = true, silent = true, desc = 'Open the last test file' },
-  },
+	"vim-test/vim-test",
+	dependencies = { "preservim/vimux" },
+	config = function()
+		vim.cmd("let test#strategy = 'vimux'")
+		vim.cmd("let VimuxUseNearest = 0")
+	end,
+	keys = {
+		{ "<leader>tn", ":TestNearest<CR>", noremap = true, silent = true, desc = "Test nearest line" },
+		{ "<leader>tf", ":TestFile<CR>", noremap = true, silent = true, desc = "Test file" },
+		{ "<leader>ts", ":TestSuite<CR>", noremap = true, silent = true, desc = "Test suite" },
+		{ "<leader>tl", ":TestLast<CR>", noremap = true, silent = true, desc = "Test last" },
+		{ "<leader>tv", ":TestVisit<CR>", noremap = true, silent = true, desc = "Open the last test file" },
+	},
 }
