e4e2eaf491742abfb9fa434d71bc1e73bdabbeaf
Ilya Timofeev <me@tmfv.ru>
2025-02-12 15:33:03 +0300

    Add close the test pane mapping

--- nvim/.config/nvim/lua/plugins/vim-test.lua
+++ nvim/.config/nvim/lua/plugins/vim-test.lua
@@ -4,6 +4,12 @@ return {
 	config = function()
 		vim.cmd("let test#strategy = 'vimux'")
 		vim.cmd("let VimuxUseNearest = 0")
+		vim.cmd([[
+      function! CloseVimuxPane()
+        call system("tmux kill-pane -t bottom")
+      endfunction
+      command! CloseTestPane call CloseVimuxPane()
+    ]])
 	end,
 	keys = {
 		{ "<leader>tn", ":TestNearest<CR>", noremap = true, silent = true, desc = "Test nearest line" },
@@ -11,5 +17,12 @@ return {
 		{ "<leader>ts", ":TestSuite<CR>", noremap = true, silent = true, desc = "Test suite" },
 		{ "<leader>tl", ":TestLast<CR>", noremap = true, silent = true, desc = "Test last" },
 		{ "<leader>tv", ":TestVisit<CR>", noremap = true, silent = true, desc = "Open the last test file" },
+		{
+			"<leader>tc",
+			":CloseTestPane<CR>",
+			noremap = true,
+			silent = true,
+			desc = "Close test pane (must be at the bottom)",
+		},
 	},
 }
