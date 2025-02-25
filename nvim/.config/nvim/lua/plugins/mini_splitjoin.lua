eaafb38d000cbd04b9cce32c3121dea7fc861d29
Ilya Timofeev <me@tmfv.ru>
2025-02-25 17:57:31 +0300

    Switch to treesj

--- nvim/.config/nvim/lua/plugins/mini_splitjoin.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"echasnovski/mini.splitjoin",
-	version = false,
-	opts = {
-		mappings = {
-			toggle = "S",
-		},
-	},
-}
---
+++ nvim/.config/nvim/lua/plugins/treesj.lua
@@ -0,0 +1,7 @@
+return {
+	"Wansmer/treesj",
+	dependencies = { "nvim-treesitter/nvim-treesitter" }, -- if you install parsers with `nvim-treesitter`
+	config = function()
+		require("treesj").setup({})
+	end,
+}
