ce1f9a9a361cded87a80f7096e32b846b1ff618a
Ilya Timofeev <me@tmfv.ru>
2025-02-25 18:55:08 +0300

    Better treesj mapping

--- nvim/.config/nvim/lua/plugins/treesj.lua
+++ nvim/.config/nvim/lua/plugins/treesj.lua
@@ -1,6 +1,17 @@
 return {
 	"Wansmer/treesj",
 	dependencies = { "nvim-treesitter/nvim-treesitter" }, -- if you install parsers with `nvim-treesitter`
+	keys = {
+		{
+			"S",
+			":lua require('treesj').toggle()<CR>",
+			noremap = true,
+			silent = true,
+			expr = false,
+			mode = "n",
+			desc = "Split Join toggle",
+		},
+	},
 	config = function()
 		require("treesj").setup({})
 	end,
