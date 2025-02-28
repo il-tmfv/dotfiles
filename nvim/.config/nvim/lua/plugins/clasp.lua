6aeb9d8b315b40b615fc9c67510f2b8c936ba1f3
Ilya Timofeev <me@tmfv.ru>
2025-02-28 17:54:28 +0300

    Remove one of clasp mappings

--- nvim/.config/nvim/lua/plugins/clasp.lua
+++ nvim/.config/nvim/lua/plugins/clasp.lua
@@ -16,18 +16,5 @@ return {
 		vim.keymap.set({ "n", "i" }, "H", function()
 			require("clasp").wrap("prev")
 		end)
-
-		-- If you want to exclude nodes whose end row is not current row
-		vim.keymap.set({ "n", "i" }, "<c-l>", function()
-			require("clasp").wrap("next", function(nodes)
-				local n = {}
-				for _, node in ipairs(nodes) do
-					if node.end_row == vim.api.nvim_win_get_cursor(0)[1] - 1 then
-						table.insert(n, node)
-					end
-				end
-				return n
-			end)
-		end)
 	end,
 }
