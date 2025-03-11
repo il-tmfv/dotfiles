2e45f3dd71c7f7e24b47d14de7425c34b66afc1c
Ilya Timofeev <me@tmfv.ru>
2025-03-11 16:12:06 +0300

    Fix clasp mappings

--- nvim/.config/nvim/lua/plugins/clasp.lua
+++ nvim/.config/nvim/lua/plugins/clasp.lua
@@ -8,12 +8,12 @@ return {
 		})
 
 		-- jumping from smallest region to largest region
-		vim.keymap.set({ "n", "i" }, "L", function()
+		vim.keymap.set({ "n" }, "L", function()
 			require("clasp").wrap("next")
 		end)
 
 		-- jumping from largest region to smallest region
-		vim.keymap.set({ "n", "i" }, "H", function()
+		vim.keymap.set({ "n" }, "H", function()
 			require("clasp").wrap("prev")
 		end)
 	end,
