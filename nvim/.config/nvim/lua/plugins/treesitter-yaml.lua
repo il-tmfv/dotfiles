f0fcf4c9043aa0cc7f51e84c30c3ecd2e69cf77b
Ilya Timofeev <me@tmfv.ru>
2026-01-24 10:52:33 +0300

    Better vim config

---
+++ nvim/.config/nvim/lua/plugins/treesitter-yaml.lua
@@ -0,0 +1,4 @@
+return {
+	"cuducos/yaml.nvim",
+	dependencies = { "nvim-treesitter/nvim-treesitter" },
+}
--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,64 +1,64 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  event = { "BufReadPost", "BufNewFile" },
-  dependencies = {
-    'cuducos/yaml.nvim',
-    'RRethy/nvim-treesitter-endwise',
-  },
-  -- lazy = false,
-  build = ":TSUpdate",
-  opts = {
-    -- A list of parser names, or "all"
-    ensure_installed = {
-      "clojure",
-      "ruby",
-      "elixir",
-      "heex",
-      "eex",
-      "javascript",
-      "typescript",
-      "tsx",
-      "scss",
-      "html",
-      "yaml",
-      "lua",
-      "vim",
-      "markdown"
-    },
-    -- Install languages synchronously (only applied to `ensure_installed`)
-    sync_install = false,
-    -- indent = {
-    --   enable = true,
-    -- },
-    -- to make it work with rainbow parenths
-    highlight = {
-      enable = true,
-      -- custom_captures = {
-      --   ["punctuation.bracket"] = "",
-      --   ["constructor"]         = "",
-      -- },
-    },
-    incremental_selection = {
-      enable = true,
-      keymaps = {
-        node_incremental = ".",
-        node_decremental = ",",
-      },
-    },
-    refactor = {
-      smart_rename = { enable = false },
-      navigation = { enable = false },
-      highlight_current_scope = { enable = false },
-      highlight_definitions = {
-        enable = true,
-        clear_on_cursor_move = true,
-      },
-    },
-    endwise = {
-      enable = true,
-    },
-  },
-  config = function(_, opts)
-    require("nvim-treesitter.configs").setup(opts)
-  end,
+	"nvim-treesitter/nvim-treesitter",
+	dependencies = {
+		"cuducos/yaml.nvim",
+		"RRethy/nvim-treesitter-endwise",
+	},
+	branch = "master",
+	lazy = false,
+	build = ":TSUpdate",
+	opts = {
+		-- A list of parser names, or "all"
+		ensure_installed = {
+			"clojure",
+			"ruby",
+			"elixir",
+			"heex",
+			"eex",
+			"javascript",
+			"typescript",
+			"tsx",
+			"scss",
+			"html",
+			"yaml",
+			"lua",
+			"vim",
+			"markdown",
+		},
+		-- Install languages synchronously (only applied to `ensure_installed`)
+		sync_install = false,
+		-- indent = {
+		--   enable = true,
+		-- },
+		-- to make it work with rainbow parenths
+		highlight = {
+			enable = true,
+			-- custom_captures = {
+			--   ["punctuation.bracket"] = "",
+			--   ["constructor"]         = "",
+			-- },
+		},
+		incremental_selection = {
+			enable = true,
+			keymaps = {
+				node_incremental = ".",
+				node_decremental = ",",
+			},
+		},
+		refactor = {
+			smart_rename = { enable = false },
+			navigation = { enable = false },
+			highlight_current_scope = { enable = false },
+			highlight_definitions = {
+				enable = true,
+				clear_on_cursor_move = true,
+			},
+		},
+		endwise = {
+			enable = true,
+		},
+	},
+	config = function(_, opts)
+		require("nvim-treesitter.configs").setup(opts)
+	end,
 }
