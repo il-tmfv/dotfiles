985969369236e2bd18dcfa78327bee4955d5decd
Ilya Timofeev <me@tmfv.ru>
2026-01-27 16:41:07 +0300

    Hotkey for codediff

--- nvim/.config/nvim/lua/plugins/codediff.lua
+++ nvim/.config/nvim/lua/plugins/codediff.lua
@@ -2,4 +2,7 @@ return {
 	"esmuellert/codediff.nvim",
 	dependencies = { "MunifTanjim/nui.nvim" },
 	cmd = "CodeDiff",
+	keys = {
+		{ "<leader>cd", "<cmd>CodeDiff<CR>", noremap = true, silent = true, desc = "Toggle CodeDiff" },
+	},
 }
